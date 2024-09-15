87fda27503f3c89a9163adbcbb4d60e48d5702fd
marcodev2131 <145700074+marcodev2131@users.noreply.github.com>
2024-09-14 21:37:23 -0300

    Criando o estado de troca de botões

--- src/styles/containers/ListaDeTarefas/index.tsx
+++ src/styles/containers/ListaDeTarefas/index.tsx
@@ -1,25 +1,53 @@
 import Tarefa from '../../../components/Tarefa'
 import { Container } from '../..'
 
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Pagar a conta de internet',
+    descricao: 'Baixar fatura no Gmail',
+    prioridade: 'urgente',
+    status: 'concluida'
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'Fazer treino B',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Tarefa 4',
+    descricao: 'Descrição da tarefa 4',
+    prioridade: 'urgente',
+    status: 'concluida'
+  },
+  {
+    titulo: 'Tarefa 5',
+    descricao: 'Descrição da tarefa 5',
+    prioridade: 'importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo; </p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+    {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
     </Container>
 )
